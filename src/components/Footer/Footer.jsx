160bad84a0c0943e4d659f750f072d15406aff2d
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-01-09 20:34:38 +0100

    feat: add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,42 @@
-import { MainContainer, Section } from "../UtilsStyledComponents";
-import s from "./Footer.module.css";
+import { MainContainer } from "../UtilsStyledComponents";
+import { FooterEl, AuthorInfoThumb } from "./Footer.styled";
+import movieDBlogo from "../../images/moviedb-logo.png";
 
 export default function Footer() {
   return (
-    <footer className={s.footer}>
-      <Section>
-        <MainContainer>
-          <h3>Footer</h3>
-          <p>Here will be info about technologies and author</p>
-        </MainContainer>
-      </Section>
-    </footer>
+    <MainContainer>
+      <FooterEl>
+        <AuthorInfoThumb>
+          <span>©</span>
+          <span>2022</span>
+          <a
+            href="https://github.com/MarinaTripetska"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="link to author's github"
+          >
+            github
+          </a>
+        </AuthorInfoThumb>
+
+        <a
+          href="https://goit.ua/fullstackonline/ua/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="link to course"
+        >
+          GoIT School
+        </a>
+
+        <a
+          href="https://www.themoviedb.org/?language=uk"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="link to data base MovieDB"
+        >
+          <img src={movieDBlogo} alt="" width="60" height="60" />
+        </a>
+      </FooterEl>
+    </MainContainer>
   );
 }
---
+++ src/components/Footer/Footer.styled.jsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const FooterEl = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  font-size: 18px;
+  line-height: 2;
+  font-weight: bold;
+  color: rgb(156, 156, 156);
+`;
+
+export const AuthorInfoThumb = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+  max-width: 170px;
+`;
